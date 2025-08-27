0941a10082e405abeb9e1ba8f60dbbbfba6626fb
David Young <david@thedavidyoung.co.uk>
2025-08-27 19:40:15 +0100

    Bring over azure captioning

--- src/config.rs
+++ src/config.rs
@@ -1,7 +1,8 @@
 use crate::{
-    DisplayMode, MAX_FONT, MAX_SUBTITLE_HEIGHT, MIN_FONT, MIN_SUBTITLE_HEIGHT,
+    DisplayMode, RunState, MAX_FONT, MAX_SUBTITLE_HEIGHT, MIN_FONT,
+    MIN_SUBTITLE_HEIGHT,
 };
-use egui::{ComboBox, RichText, Slider, Ui};
+use egui::{Button, ComboBox, RichText, Slider, Ui};
 
 pub fn show(ui: &mut Ui, app: &mut crate::MyApp) {
     ui.heading(RichText::new("game").size(50.0));
@@ -17,10 +18,10 @@ pub fn show(ui: &mut Ui, app: &mut crate::MyApp) {
         .text("Subtitle height"),
     );
 
-    ui.checkbox(&mut app.dark_mode_requested, "Dark Mode");
+    ui.checkbox(&mut app.dark_mode_requested, "Dark Mode [d]");
 
     ui.horizontal(|ui| {
-        ui.label("Display mode");
+        ui.label("Display mode [m]");
         ComboBox::from_id_salt("display_mode")
             .selected_text(format!("{:?}", app.display_mode))
             .show_ui(ui, |ui| {
@@ -35,5 +36,48 @@ pub fn show(ui: &mut Ui, app: &mut crate::MyApp) {
                     "Subtitle",
                 );
             });
+    });
+
+    ui.horizontal(|ui| {
+        let current_wordlist = app.wordlist.as_deref().unwrap_or("None");
+        let before = app.wordlist.clone();
+        ui.label("Wordlist");
+        ComboBox::from_id_salt("wordlist")
+            .selected_text(current_wordlist)
+            .show_ui(ui, |ui| {
+                ui.selectable_value(&mut app.wordlist, None, "None");
+                for option in &app.wordlist_options {
+                    ui.selectable_value(
+                        &mut app.wordlist,
+                        Some(option.clone()),
+                        option.as_ref(),
+                    );
+                }
+            });
+        if app.wordlist != before {
+            app.update_wordlist();
+        }
+    });
+
+    ui.horizontal(|ui| {
+        if ui
+            .add(
+                Button::new("Run [space]")
+                    .selected(app.run_state == RunState::Running),
+            )
+            .clicked()
+        {
+            app.toggle_running();
+        }
+
+        if ui
+            .add(
+                Button::new("Test [t]")
+                    .selected(app.run_state == RunState::Test),
+            )
+            .clicked()
+        {
+            app.toggle_test_mode();
+        }
     });
 }
